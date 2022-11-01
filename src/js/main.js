81a9b633791e02e1f8b2c52f1b42bfe3cbad4478
Dopamine424 <fomin02@inbox.ru>
2022-11-01 17:00:55 +0500

    anim-com-1

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,43 @@
 
 // слайдер 1
 
+
+const animItems = document.querySelectorAll('.anim');
+
+if (anim.length > 0){
+  document.addEventListener('scroll', animOnScroll);
+  function animOnScroll() {
+    for(let index = 0; index < animItems.length; index++){
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if(animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)){
+        animItem.classList.add('animate__animated');
+      }
+      else{
+        animItem.classList.remove('animate__animated');
+      }
+    }
+  }
+  function offset(el){
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = windiw.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+  }
+}
+
+
+
+
 $(document).ready(function(){
 
     let back = document.getElementById('back');
