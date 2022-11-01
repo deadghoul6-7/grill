71bca4b872c9c0109c15fe6ec65089ec322d2af3
Dopamine424 <fomin02@inbox.ru>
2022-11-01 22:17:17 +0500

    anim-com-2

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,17 @@
 
-// слайдер 1
-
-
+// animate on scroll
 const animItems = document.querySelectorAll('.anim');
 
-if (anim.length > 0){
-  document.addEventListener('scroll', animOnScroll);
-  function animOnScroll() {
+if (animItems.length > 0){
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll(event) {
     for(let index = 0; index < animItems.length; index++){
       const animItem = animItems[index];
       const animItemHeight = animItem.offsetHeight;
       const animItemOffset = offset(animItem).top;
       const animStart = 4;
 
-      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      let animItemPoint = window.innerHeight - (animItemHeight / animStart);
 
       if(animItemHeight > window.innerHeight) {
         animItemPoint = window.innerHeight - window.innerHeight / animStart;
@@ -29,12 +27,53 @@ if (anim.length > 0){
   }
   function offset(el){
     const rect = el.getBoundingClientRect(),
-      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-      scrollTop = windiw.pageYOffset || document.documentElement.scrollTop;
+      scrollLeft = el.offsetX || document.documentElement.scrollLeft,
+      scrollTop = el.offsetY || document.documentElement.scrollTop;
       return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
   }
+  animOnScroll();
 }
 
+// delay elements
+
+const item__time = document.querySelector('.item__time');
+item__time.style.setProperty('--animate-delay', '0.3s');
+
+const item__email = document.querySelector('.item__email');
+item__email.style.setProperty('--animate-delay', '0.4s');
+
+const phone__block = document.querySelector('.phone-block');
+phone__block.style.setProperty('--animate-delay', '0.4s');
+
+const icons = document.querySelector('.icons');
+icons.style.setProperty('--animate-delay', '0.3s');
+
+const main__text__in__block__1 = document.querySelector('.main-text-in-block-1');
+main__text__in__block__1.style.setProperty('--animate-delay', '0.5s');
+
+const links__block__1__a2 = document.querySelector('.links__block__1__a2');
+links__block__1__a2.style.setProperty('--animate-delay', '0.8s');
+
+const links__block__1__a3 = document.querySelector('.links__block__1__a3');
+links__block__1__a3.style.setProperty('--animate-delay', '0.6s');
+
+const line__block__1 = document.querySelector('.line-block-1');
+line__block__1.style.setProperty('--animate-delay', '1.2s');
+
+const moving__stick__block__1 = document.querySelector('.moving-stick-block-1');
+moving__stick__block__1.style.setProperty('--animate-delay', '1.2s');
+
+const elem__1__block__1 = document.getElementById('elem-1-block-1');
+elem__1__block__1.style.setProperty('--animate-delay', '1.3s');
+
+const elem__2__block__1 = document.getElementById('elem-2-block-1');
+elem__2__block__1.style.setProperty('--animate-delay', '1.4s');
+
+const elem__3__block__1 = document.getElementById('elem-3-block-1');
+elem__3__block__1.style.setProperty('--animate-delay', '1.5s');
+
+const elem__4__block__1 = document.getElementById('elem-4-block-1');
+elem__4__block__1.style.setProperty('--animate-delay', '1.6s');
 
 
 
