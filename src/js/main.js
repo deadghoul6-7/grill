1c11be5702ceeb35590685e399608186e5e09e8e
Dopamine424 <fomin02@inbox.ru>
2022-11-24 19:38:35 +0500

    block-9-6-com-2

--- src/js/main.js
+++ src/js/main.js
@@ -134,6 +134,60 @@ settings: {
 // instead of a settings object
 ]
 });
+
+
+$('.slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  adaptiveHeight: false,
+  prevArrow: $('.prev-arrow-block7'),
+  nextArrow: $('.next-arrow-block7'),
+  responsive: [
+  {
+  breakpoint: 1024,
+  settings: {
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    infinite: false,
+    adaptiveHeight: false,
+    prevArrow: $('.prev-arrow-block7'),
+    nextArrow: $('.next-arrow-block7'),
+    dots: false
+  }
+  },
+  {
+  breakpoint: 600,
+  settings: {
+    slidesToShow: 2,
+    adaptiveHeight: false,
+    prevArrow: $('.prev-arrow-block7'),
+    nextArrow: $('.next-arrow-block7'),
+    slidesToScroll: 2
+    
+  }
+  },
+  {
+  breakpoint: 480,
+  settings: {
+    slidesToShow: 1,
+    adaptiveHeight: false,
+    prevArrow: $('.prev-arrow-block7'),
+    nextArrow: $('.next-arrow-block7'),
+    slidesToScroll: 1
+  }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+  ]
+  });
+
+
+
+
   });
   $(document).ready(function(){
     $('.slider-block7').slick({
